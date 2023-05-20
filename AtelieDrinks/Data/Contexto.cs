ec8c625f974e4155425b3cd62c69bebbfe6c9ff2
BRUNOMTK <brunomendestk@ufu.br>
2023-05-20 19:41:35 -0300

    [Realese][done]Codigo Funciondo

---
+++ AtelieDrinks/Data/Contexto.cs
@@ -0,0 +1,57 @@
+﻿using AtelieDrinks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtelieDrinks.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Base_alcoolica> Base_Alcoolica { get; set; }
+        public DbSet<Custo_deslocamento> Custo_Deslocamento { get; set; }
+        public DbSet<Custo_operacional> Custo_operacional { get; set; }
+        public DbSet<Deposito> Deposito { get; set; }
+        public DbSet<Drinks> Drinks { get; set; }
+        public DbSet<Ficha_tecnica> Ficha_tecnica { get; set; }
+        public DbSet<Historico> Historico { get; set; }
+        public DbSet<Insumos> Insumos { get; set; }
+        public DbSet<Marca> Marca { get; set; }
+        public DbSet<Orcamento> Orcamento { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Base_alcoolica>()
+                .HasKey(d => d.id_base_alcoolica);
+
+            modelBuilder.Entity<Custo_deslocamento>()
+                .HasKey(d => d.id_taxa_deslocamento);
+
+            modelBuilder.Entity<Custo_operacional>()
+                .HasKey(d => d.id_custo_operacional);
+
+            modelBuilder.Entity<Deposito>()
+                .HasKey(d => d.id_item);
+
+            modelBuilder.Entity<Drinks>()
+                .HasKey(d => d.id_drink);
+
+            modelBuilder.Entity<Ficha_tecnica>()
+                .HasKey(d => d.id_ficha);
+
+            modelBuilder.Entity<Historico>()
+                .HasKey(d => d.id_historico);
+
+            modelBuilder.Entity<Insumos>()
+                .HasKey(d => d.id_insumo);
+
+            modelBuilder.Entity<Marca>()
+                .HasKey(d => d.id_marca);
+
+            modelBuilder.Entity<Orcamento>()
+                .HasKey(d => d.id_orcamento);
+
+        }
+    }
+}
---
+++ AtelieDrinks/Models/Base_alcoolica.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AtelieDrinks.Models
+{
+    [Table("Base_alcoolica")]
+    public class Base_alcoolica
+    {
+        [Key]
+        [Column("id_base_alcoolica")]
+        [Display(Name = "id_base_alcoolica")]
+        public int id_base_alcoolica { get; set; }
+
+        [Column("quantidade")]
+        [Display(Name = "Quantidade")]
+        public int quantidade { get; set; }
+
+        [Column("nome_bebida")]
+        [Display(Name = "Nome da bebida")]
+        public string nome_bebida { get; set; }
+
+        [Column("nome_marca")]
+        [Display(Name = "Nome da marca")]
+        public string nome_marca { get; set; }
+
+        [Column("custo_garrafa")]
+        [Display(Name = "Custo garrafa")]
+        public decimal custo_garrafa { get; set; }
+
+        [Column("custo_total")]
+        [Display(Name = "Custo total")]
+        public decimal custo_total { get; set; }
+
+        [Column("id_bebida")]
+        [Display(Name = "id_bebida")]
+        public Marca? id_bebida { get; set; }
+
+        [Column("id_marca")]
+        [Display(Name = "id_marca")]
+        public Marca? id_marca { get; set; }
+    }
+}
